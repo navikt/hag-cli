87b4cfd8f45cc08df977c241fcb3674060022189
Mikael Bjerga <mikael.bjerga@nav.no>
2025-09-08 13:55:14 +0200

    Synk fork

--- fetch-keystores.sh
+++ fetch-keystores.sh
@@ -1,22 +1,17 @@
 #!/bin/zsh
 
-KEYTSTORE_PROD_NAME=aiven-prod-keystore.p12
+KEYSTORE_PROD_NAME=aiven-prod-keystore.p12
 TRUSTORE_PROD_NAME=aiven-prod-truststore.jks
-KEYTSTORE_DEV_NAME=aiven-dev-keystore.p12
+KEYSTORE_DEV_NAME=aiven-dev-keystore.p12
 TRUSTORE_DEV_NAME=aiven-dev-truststore.jks
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-alias k=kubectl
-function kx() {
-    k config use-context $1
-}
 
 function checkKeystore() {
   if ! keytool -list -v -keystore "$1" -storepass changeme -storetype PKCS12 > /dev/null;
   then
       echo Keystore is corrupt, or bad password
   else
-      echo Keystore $1 seem to be ok ...
+      echo Keystore "$1" seem to be ok ...
   fi
 }
 
@@ -25,37 +20,51 @@ function checkTruststore() {
   then
       echo Truststore is corrupt, or bad password
   else
-  	echo Trustore $1 seem to be ok ...
+  	echo Trustore "$1" seem to be ok ...
   fi
 }
 
 function getSecrets() {
-    BROKERS=$(k get secret "$1" -n helsearbeidsgiver -o jsonpath='{.data.KAFKA_BROKERS}' | base64 -D)
+    local -r secretToGet=$1
+    local -r keystoreName=$2
+    local -r truststoreName=$3
+    local -r filename=$4
+    BROKERS=$(k get secret "$secretToGet" -n helsearbeidsgiver -o jsonpath='{.data.KAFKA_BROKERS}' | base64 -D)
     echo "Brokers: $BROKERS"
-    k get secret "$1" -n helsearbeidsgiver -o jsonpath='{.data.client\.keystore\.p12}' | tee "$2.base64" | base64 -D > "$2"
-    k get secret "$1" -n helsearbeidsgiver -o jsonpath='{.data.client\.truststore\.jks}' | tee "$3.base64" | base64 -D > "$3"
-    checkKeystore "$2"
-    checkTruststore "$3"
+    k get secret "$secretToGet" -n helsearbeidsgiver -o jsonpath='{.data.client\.keystore\.p12}' | tee "$keystoreName.base64" | base64 -D > "$keystoreName"
+    k get secret "$secretToGet" -n helsearbeidsgiver -o jsonpath='{.data.client\.truststore\.jks}' | tee "$truststoreName.base64" | base64 -D > "$truststoreName"
+    checkKeystore "$keystoreName"
+    checkTruststore "$truststoreName"
 
-    echo "Writing properties to $4"
-    echo "config = aiven" > "$4"
-    echo "aiven.brokers.url = $BROKERS" >> "$4"
-    echo "aiven.truststore.path = $(pwd)/$3" >> "$4"
-    echo "aiven.truststore.password = changeme" >> "$4"
-    echo "aiven.keystore.path = $(pwd)/$2" >> "$4"
-    echo "aiven.keystore.password = changeme" >> "$4"
+    echo "Writing properties to $filename"
+    {
+        echo "config = aiven"
+        echo "aiven.brokers.url = $BROKERS"
+        echo "aiven.truststore.path = $(pwd)/$truststoreName"
+        echo "aiven.truststore.password = changeme"
+        echo "aiven.keystore.path = $(pwd)/$keystoreName"
+        echo "aiven.keystore.password = changeme"
+    } > "$filename"
 }
 
-if test "$#" -eq 0; then
-    echo "Usage: $0 <prod secret name> [<dev secret name>]"
-    echo "You must provide prod secret name."
-    exit 1
-fi
+function validateArgs() {
+    if test -z "$1"; then
+        echo "Usage: $ZSH_ARGZERO [--dev] <secret name>"
+        echo "You must provide secret name."
+        exit 1
+    fi
+}
 
-kx prod-gcp
-getSecrets $1 $KEYTSTORE_PROD_NAME $TRUSTORE_PROD_NAME "$SCRIPT_DIR/config/prod-aiven.properties"
+declare kontext=prod-gcp
+function k() {
+    kubectl --context $kontext "$@"
+}
 
-if ! test -z "$2"; then
-    kx dev-gcp
-    getSecrets $2 $KEYTSTORE_DEV_NAME $TRUSTORE_DEV_NAME "$SCRIPT_DIR/config/dev-aiven.properties"
+if test "$1" = --dev; then
+    validateArgs "$2"
+    kontext=dev-gcp
+    getSecrets "$2" $KEYSTORE_DEV_NAME $TRUSTORE_DEV_NAME "$SCRIPT_DIR/config/dev-aiven.properties"
+else
+    validateArgs "$1"
+    getSecrets "$1" $KEYSTORE_PROD_NAME $TRUSTORE_PROD_NAME "$SCRIPT_DIR/config/prod-aiven.properties"
 fi

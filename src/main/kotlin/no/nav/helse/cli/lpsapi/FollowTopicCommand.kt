1d0e45525222ff8cf11c6e42a7182762b65596e6
Mehdi.Zare <mehdi.zare@nav.no>
2025-09-22 12:35:26 +0200

    Jobb for å finne manglende forespøsler i LPS-API

---
+++ src/main/kotlin/no/nav/helse/cli/lpsapi/FollowTopicCommand.kt
@@ -0,0 +1,35 @@
+package no.nav.helse.cli.lpsapi
+
+import no.nav.helse.cli.Command
+import no.nav.rapids_and_rivers.cli.ConsumerProducerFactory
+import no.nav.rapids_and_rivers.cli.JsonRiver
+import no.nav.rapids_and_rivers.cli.RapidsCliApplication
+import kotlin.random.Random
+
+internal class FollowTopicCommand : Command {
+    override val name = "follow_topic"
+
+    override fun usage() {
+        println("Usage: $name <topic>")
+        println("Prints all messages from a topic")
+    }
+
+    override fun execute(factory: ConsumerProducerFactory, args: List<String>) {
+        if (args.isEmpty()) throw RuntimeException("Missing required topic arg")
+        val topic = args[0]
+        val groupId = "hag-cli-${Random.Default.nextInt()}"
+
+        println("Consuming from $topic with consumer group $groupId")
+        println("Will NOT be committing any offsets. Starting from LATEST")
+        RapidsCliApplication(factory)
+            .apply {
+                JsonRiver(this).onMessage { record, node ->
+                    println("key=${record.key()}")
+                    println("#${record.partition()}, offset ${record.offset()} - ${node.path("@event_name").asText()}: ${node.path("@id").asText()} --> ${node.toString()}")
+                }
+            }
+            .start(groupId, listOf(topic))
+    }
+
+    override fun verify(factory: ConsumerProducerFactory) {}
+}
